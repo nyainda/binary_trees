c572cd85b67372ddc35bf3782e73fb5fd4d78e86
bruce oyugi <oyugibruce2017@gmail.com>
2022-08-25 02:24:09 +0300

    0x1D. C - Binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,12 +9,12 @@
   */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *binary_tree_node = NULL;
+	binary_tree_t *bt = NULL;
 
-	binary_tree_node = calloc(1, sizeof(binary_tree_t));
-	if (!binary_tree_node)
+	bt = calloc(1, sizeof(binary_tree_t));
+	if (!bt)
 		return (NULL);
-	binary_tree_node->parent = parent;
-	binary_tree_node->n = value;
-	return (binary_tree_node);
+	bt->parent = parent;
+	bt->n = value;
+	return (bt);
 }
